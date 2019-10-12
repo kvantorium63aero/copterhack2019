bc2fa1d995e2b3f37e1b37920d821f023bb44979
IgorGoryainov <44380441+IgorGoryainov@users.noreply.github.com>
2019-10-12 23:13:22 +0300

    Update flight.py

--- flight.py
+++ flight.py
@@ -4,6 +4,7 @@ from clever import srv
 from std_srvs.srv import Trigger
 from clever.srv import SetLEDEffect
 import math
+import decode_from_txt2points
 
 rospy.init_node('flight')
 
@@ -18,9 +19,12 @@ land = rospy.ServiceProxy('land', Trigger)
 
 set_effect = rospy.ServiceProxy('led/set_effect', SetLEDEffect)  # define proxy to ROS-service
 
-def navigate_wait(x, y, z, speed, frame_id, tolerance=0.5):
-    navigate(x=x, y=y, z=z, speed=speed, frame_id=frame_id, yaw=1.57)
 
+mas = decode_from_txt2points.decoderOK()
+
+
+def navigate_wait(x, y, z, speed, frame_id, tolerance=0.3):
+    navigate(x=x, y=y, z=z, speed=speed, frame_id=frame_id, yaw=float('nan'))
     while not rospy.is_shutdown():
         telem = get_telemetry(frame_id='navigate_target')
         if math.sqrt(telem.x ** 2 + telem.y ** 2 + telem.z ** 2) < tolerance:
@@ -34,7 +38,7 @@ s = 0.5 #speed
 for i in range(2):
 	print("NEXT CUB - GO!")
 	#set_effect(r=255, g=255, b=0)
-	set_effect(effect='rainbow_fill')
+	set_effect(r = 255, g = 0, b = 255)
 	navigate_wait(x=2, y=0, z=2.5, speed=s, frame_id='aruco_map')
 	navigate_wait(x=2, y=2, z=2.5, speed=s, frame_id='aruco_map')
 	navigate_wait(x=0, y=2, z=2.5, speed=s, frame_id='aruco_map')
