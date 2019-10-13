c6e56858063afe694d05b5306b4da74d1fccebd8
IgorGoryainov <44380441+IgorGoryainov@users.noreply.github.com>
2019-10-13 14:30:40 +0300

    Update main_flight.py

--- main_flight.py
+++ main_flight.py
@@ -4,35 +4,15 @@
 import rospy
 import cv2
 import math
-from sensor_msgs.msg import Image
-from std_msgs.msg import Bool
-from cv_bridge import CvBridge
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.utils import get_random_id
 from urllib.request import urlopen
 import time
-from clever import srv
-from std_srvs.srv import Trigger
-from clever.srv import SetLEDEffect
 import decode_from_txt2points
 import flight
-rospy.init_node('flight')
-
-get_telemetry = rospy.ServiceProxy('get_telemetry', srv.GetTelemetry)
-navigate = rospy.ServiceProxy('navigate', srv.Navigate)
-navigate_global = rospy.ServiceProxy('navigate_global', srv.NavigateGlobal)
-set_position = rospy.ServiceProxy('set_position', srv.SetPosition)
-set_velocity = rospy.ServiceProxy('set_velocity', srv.SetVelocity)
-set_attitude = rospy.ServiceProxy('set_attitude', srv.SetAttitude)
-set_rates = rospy.ServiceProxy('set_rates', srv.SetRates)
-land = rospy.ServiceProxy('land', Trigger)
-
-set_effect = rospy.ServiceProxy('led/set_effect', SetLEDEffect)  # define proxy to ROS-service
-
 
 def main():
-    rospy.init_node('bot')
     vk_session = vk_api.VkApi(token='TOKEN')
     vk = vk_session.get_api()
     longpoll = VkBotLongPoll(vk_session, '187507235')
